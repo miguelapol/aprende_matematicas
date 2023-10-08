b5c6b416430e8df7cc3ae1111859c168d988230a
Miguel Angel <33709574+miguelapol@users.noreply.github.com>
2023-10-08 12:56:50 -0600

    cronometro

--- Assets/Scripts/multiplicar/multiple.cs
+++ Assets/Scripts/multiplicar/multiple.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,19 +6,23 @@ public class multiple : MonoBehaviour
 {
     public Text pregunta;
     public Text[] respuestas;
+    public Text cronometrotexto;
 
     int numero1;
     int numero2;
     string preguntaFinal;
     int producto;
     int aleatorio_respuesta;
+    public int  tiempoCronometro;
+    private float contador;
     void Start()
     {
-
+        contador = tiempoCronometro;
+        cronometrotexto.text = contador.ToString();
         generadorNumero();
 
     }
- void generadorNumero()
+    void generadorNumero()
     {
         numero1 = Random.Range(2, 11);
         numero2 = Random.Range(2, 11);
@@ -39,8 +44,23 @@ public class multiple : MonoBehaviour
             }
         }
     }
+    //contador conometro
+  
     void Update()
     {
+    //el cronometro se ejecuta todo el juego
+    //deltatime mide el tiempo que pasa entre cada frame 
+    contador =contador - Time.deltaTime;
+     cronometrotexto.text = contador.ToString("f0");
+        if(contador <= 0)
+        {
+        Debug.Log("se acabo el tiempo");
+        generadorNumero();
+        //se hace para que el contado se hace 5 segundos
+        contador = tiempoCronometro;
+        cronometrotexto.text = contador.ToString();
+                
 
+        }
     }
 }
