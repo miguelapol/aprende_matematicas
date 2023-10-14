1e50767a3aa763f04f8c57ecefcd1f74bad5c07e
Miguel Angel <33709574+miguelapol@users.noreply.github.com>
2023-10-14 11:19:43 -0600

    script
    nivel1

--- Assets/Scripts/niveles.cs
+++ Assets/Scripts/niveles.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class niveles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Jugar(string SampleScene)
     {
-        
+        SceneManager.LoadScene(SampleScene);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
